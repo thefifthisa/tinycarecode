032a81176d84d8d8b1d2e008d60116a070e2e306
thefifthisa <ijlavandero@gmail.com>
2018-06-25 13:06:51 +0800

    refresh alert and status bar every hour

--- extension.js
+++ extension.js
@@ -1,12 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-let Twit = require('twit');
-let config = require('./config.js');
+const Twit = require('twit');
+const config = require('./config.js');
+const REFRESH_AFTER = 60; // minutes til new alert pops up
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-function activate(context) {
+function activate (context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -24,9 +25,9 @@ function activate(context) {
     }
 
     let tinyCare = {
-        statusBarItem: vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left),
+        statusBarItem: vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right),
         getTweet: function () {
-            return new Promise (function (resolve, reject) {
+            return new Promise(function (resolve, reject) {
                 T.get('statuses/user_timeline', params, function (err, data) {
                     if (err) {
                         console.log(err);
@@ -37,29 +38,29 @@ function activate(context) {
                 });
             });
         },
-        updateStatusBar: function(status) {
+        sendAlert: function (status) {
+            vscode.window.showInformationMessage(status);
+        },
+        updateStatusBar: function (status) {
             this.statusBarItem.text = status;
             this.statusBarItem.show();
         }
     }
 
-    let disposable = vscode.commands.registerCommand('extension.tinyCare', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
+    setInterval(function () {
         tinyCare.getTweet().then(function (status) {
+            tinyCare.sendAlert(status);
             tinyCare.updateStatusBar(status);
         });
-    });
+    }, REFRESH_AFTER * 60 * 1000);
 
     context.subscriptions.push(T);
     context.subscriptions.push(params);
     context.subscriptions.push(tinyCare);
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
+function deactivate () {
 }
 exports.deactivate = deactivate;
---
+++ sample_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'aaaa',
+    consumer_secret: 'bbbb',
+    access_token: 'cccc',
+    access_token_secret: 'dddd'
+}
